08a5f5394962d323e55e2bbfa37a2958acd7c510
407 <407@academicos.cesde.edu.co>
2022-11-29 16:39:01 -0500

    se crea proyecto

---
+++ src/Home/Home.js
@@ -0,0 +1,54 @@
+ import './Home.css'
+ export function Home(){
+
+    return(
+        <>
+            <section>
+                <div class="banner">
+
+                </div>
+            </section>
+            <section>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-md-8">
+                            <h2>COLDPLAY</h2>
+                            <p>Banda de pop rock fundada en Londres, Inglaterra. Todo surgió cuando Chris Martin y Jonny Buckland se conocieron en la Universidad de Londres en 1996, luego de un tiempo pensando en conformar una banda lograron consolidar Pectoralz. Posteriormente se unió al proyecto musical Guy Berryman. Un año después la banda fue renombrada Starfish, para ese momento ya realizaban algunas presentaciones en clubes. El siguiente en ingresar a la agrupación fue Phil Harvey, estudiante de Oxford, que luego se convirtió en el mánager de la banda. Ahora bien, la formación original fue completada con Will Champion, encargado de la percusión.
+                            Luego de un tiempo se les convenció para que cambiaran su nombre, adoptando finalmente el nombre Coldplay. En un fin de semana en Virginia Water Martin conoció a Tim Rice-Oxley, y se dio cuenta de su gran talento en el piano, por ello le pidió ser el teclista de Coldplay, pero este se negó pues ya tenía su propia banda, Keane. Continuaron en la búsqueda de un integrante para el piano. En 1998, la banda lanzó quinientas copias del EP Safety. De estas quinientas la gran mayoría se regalaron a compañías discográficas, amigos y familiares.</p>
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <i class="bi bi-vinyl-fill fs-1"></i>
+                            <i class="bi bi-vinyl-fill fs-1"></i>
+                            <i class="bi bi-vinyl-fill fs-1"></i>
+                            <i class="bi bi-vinyl-fill fs-1"></i>
+                        </div>
+                            <div class="imagen">
+                            
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12 col-md-4">
+
+                            <img src="https://firebasestorage.googleapis.com/v0/b/spotylflm.appspot.com/o/maxresdefault.jpg?alt=media&token=8ce1084d-78a5-4094-92fb-d5592994b07d" alt="" class="img-fluid w-100"/>
+
+                        </div>
+                        <div class="col-12 col-md-4">
+
+                            <img src="https://firebasestorage.googleapis.com/v0/b/spotylflm.appspot.com/o/maxresdefault.jpg?alt=media&token=8ce1084d-78a5-4094-92fb-d5592994b07d" alt="" class="img-fluid w-100"/>
+
+                        </div>
+                        <div class="col-12 col-md-4">
+
+                            <img src="https://firebasestorage.googleapis.com/v0/b/spotylflm.appspot.com/o/maxresdefault.jpg?alt=media&token=8ce1084d-78a5-4094-92fb-d5592994b07d" alt="" class="img-fluid w-100"/>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+ }

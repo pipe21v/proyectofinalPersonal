58b1949134846656fb9608639a4ca013956629b9
405 <405@academicos.cesde.edu.co>
2022-12-13 11:27:48 -0500

    se actualiza proyecto

--- src/Ampliarinfo/Ampliarinfo.js
+++ src/Ampliarinfo/Ampliarinfo.js
@@ -1,3 +1,4 @@
+import './Ampliarinfo.css'
 import { useLocation } from "react-router-dom"
 export function Ampliarinfo(){
 
@@ -5,11 +6,42 @@ export function Ampliarinfo(){
 
 
     let producto=datosrecibidos.state.productoSeleccionado
+    
     console.log(producto)
 
     return(
         <>
-            <h2>{producto.nombre}</h2>
+            <div className="container">
+
+                <div className="row my-5">
+                    <div className="col-12 col-md-8">
+                        <img src={producto.foto} alt="foto" className="img-fluid w-100"/>
+                    </div>
+                    <div className="col-12 col-md-4 border p-3">
+                        <h2 className="fw-bold">{producto.nombre}</h2>
+                        <h1 className="mt-3 display-2">{producto.precio}<span className='descuento'>20% off</span></h1>
+                        <br/>
+                        <span class="badge text-light bg-info">Descuento del día</span>
+                        <p>Hasta 48 cuotas</p>
+                        <img src='https://firebasestorage.googleapis.com/v0/b/spotylflm.appspot.com/o/visa.png?alt=media&token=a2dfddd6-b808-4c6f-bdb9-b7a5ea93b16a' alt='' className='img-fluid'></img>
+                        <img src='https://firebasestorage.googleapis.com/v0/b/spotylflm.appspot.com/o/american-express%20(1).png?alt=media&token=262ad728-54a5-42ed-9113-caf643787f8a' alt='' className='img-fluid ms-2'></img>
+
+                        <br/>
+                        <img src='https://firebasestorage.googleapis.com/v0/b/spotylflm.appspot.com/o/delivery.png?alt=media&token=36c38ca2-4079-444b-9ead-b27cabf8299e' alt='' className='img-fluid mt-5 fs-1'></img>
+                        <p className='fw-bold'> Envio gratis a nivel nacional </p>
+                        <p>conoce los tiempos y las formas de envío</p>
+                        <i class="bi bi-arrow-clockwise mt-5 fs-1 text-success"></i>
+                        <p className='fw-bold text-success'> Devolución gratis </p>
+                        <p>30 días para reembolso</p>
+                        <br/>
+                        <h1 className='fw-bold fs-5'>Descripción</h1>
+                        <p className='mt-3 display-2 fs-5'>{producto.descripcion}</p>
+
+
+                    </div>
+                </div>
+
+            </div>
         </>
     )
 }
--- src/Mercancia/Mercancia.js
+++ src/Mercancia/Mercancia.js
@@ -19,32 +19,38 @@ export function Mercancia(){
         {
             nombre:"Camiseta Music of the Spheres",
             foto:"https://firebasestorage.googleapis.com/v0/b/spotylflm.appspot.com/o/producto1.webp?alt=media&token=89f9bfa4-d050-4fea-b50e-b31494ab6d9b",
-            precio: " $ 60.000"
+            precio: " $ 60.000",
+            descripcion: "DISEÑO DE LA GIRA MUNDIAL DE LA MÚSICA DE LAS ESFERAS IMPRESO EN UNA CAMISETA NEGRA UNISEX CON TINTAS SOSTENIBLES, 100% ALGODÓN."
         },
         {
             nombre:"Brazalete Music of The Spheres",
             foto:"https://firebasestorage.googleapis.com/v0/b/spotylflm.appspot.com/o/producto2.webp?alt=media&token=07d96c75-15a4-48f9-b564-d1ce5d13e940",
-            precio:"$ 30.000"
+            precio:"$ 30.000",
+            descripcion: "MÚSICA DE LAS ESFERAS - PULSERA TEJIDA, 100% HILOS DE ALGODÓN. CERTIFICADO FSC, EMBALAJE DE TARJETA RECICLADA"
         },
         {
             nombre:"Camiseta Music of the Spheres Purple Tour",
             foto:"https://firebasestorage.googleapis.com/v0/b/spotylflm.appspot.com/o/producto3.webp?alt=media&token=8d12f5bb-3ed6-450e-a938-cbc4a3ff0634",
-            precio:" $ 60.000"
+            precio:" $ 60.000",
+            descripcion: "¡EXCLUSIVO EN LÍNEA! DISEÑO DE LA GIRA MUNDIAL DE LA MÚSICA DE LAS ESFERAS IMPRESO EN UNA CAMISETA UNISEX MORADA CON TINTAS SOSTENIBLES, 100% ALGODÓN."
         },
         {
             nombre:"Buso Holiday Crewneck",
             foto:"https://firebasestorage.googleapis.com/v0/b/spotylflm.appspot.com/o/producto4.webp?alt=media&token=eb19f716-d802-4e74-a33a-e966a529db7b",
-            precio:" $ 120.000"
+            precio:" $ 120.000",
+            descripcion: "DISEÑO DE SUÉTER NAVIDEÑO ESTAMPADO EN UNA SUDADERA UNISEX DE CUELLO REDONDO 100% ALGODÓN ORGÁNICO."
         },
         {
             nombre:"Medias Mylo Xyloto",
             foto:"https://firebasestorage.googleapis.com/v0/b/spotylflm.appspot.com/o/producto5.webp?alt=media&token=aee1855a-d182-47f2-aad6-7212cad5f288",
-            precio:" $ 25.000"
+            precio:" $ 25.000",
+            descripcion: "UN PAR DE CALCETINES DE DISEÑO MYLO XYLOTO 70 % POLIÉSTER RECICLADO, 15 % NAILON RECICLADO Y 15 % ELÁSTICO HECHO EN LOS EE. UU"
         },
         {
             nombre:"Gorra Original Coldplay",
             foto:"https://firebasestorage.googleapis.com/v0/b/spotylflm.appspot.com/o/producto6.webp?alt=media&token=d2c1db5c-39e7-426c-b216-0d8b4bb08126",
-            precio:" $ 70.000"
+            precio:" $ 70.000",
+            descripcion: "GORRA NEGRA BORDADA CON LOGO DE COLDPLAY Y LUNA. LA GORRA ES 100% ALGODÓN, AJUSTABLE, TALLA ÚNICA. "
         }
     ]
 
@@ -67,6 +73,7 @@ export function Mercancia(){
                                             <img src={producto.foto} alt="foto" className="h-100 img-fluid w-100"/>
                                             <h4 className="text-center">{producto.nombre}</h4>
                                             <h4 className="text-center">{producto.precio}</h4>
+                    
 
 
                                             <button className="btn btn-primary mx-3 my-3" onClick={
